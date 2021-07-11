8e9c6406c8db7bdecb368ce209923a5f3f47df2e
Pramesh Karki <Parameswarkarki@gmail.com>
2021-07-11 21:35:13 +0545

    Initial project setup

---
+++ src/App.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const App=()=>{
+return <h1>Food Order App</h1> 
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+// *Import Root Component
+import App from "./App";
+
+import "./index.css";
+
+// *Render root component in DOM
+ReactDOM.render(<App />,document.getElementById("root"));
